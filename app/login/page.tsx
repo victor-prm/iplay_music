7fb7ec6a85541ec5d15c430e618b377495bcc881
victor-prm <victorpabb@gmail.com>
2026-01-13 12:29:14 +0100

    done with basic auth example

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=playlist-read-private`
+
+
+export default function Page() {
+    return (
+        <a href={url}>
+            Log in with Spotify
+        </a>
+    )
+}
