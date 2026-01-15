89224101083c52e223858fe0d3419c8a3b3d67a5
victor-prm <victorpabb@gmail.com>
2026-01-15 09:45:57 +0100

    chore: more cleanup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,20 @@
-const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URI = process.env.REDIRECT_URI;
+const CLIENT_ID = process.env.CLIENT_ID!;
+const REDIRECT_URI = process.env.REDIRECT_URI!;
 
-const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=playlist-read-private`
+// Include all the scopes your app needs
+const SCOPES = [
+  "playlist-read-private",
+  "user-read-private",
+  "user-library-read",
+  "user-follow-read"
+].join(" "); // space-separated for Spotify
 
+const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPES)}`;
 
 export default function Page() {
-    return (
-        <a href={url} className="font-headline">
-            Log in with Spotify
-        </a>
-    )
+  return (
+    <a href={url} className="font-headline">
+      Log in with Spotify
+    </a>
+  );
 }
