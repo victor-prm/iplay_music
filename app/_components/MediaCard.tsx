d7367db6b8b8a56ac7f70a77a18a51f6fbe653fd
victor-prm <victorpabb@gmail.com>
2026-01-21 14:28:32 +0100

    chore: add fallback when no artist image

--- app/_components/MediaCard.tsx
+++ app/_components/MediaCard.tsx
@@ -3,56 +3,59 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
+import { FaMusic } from "react-icons/fa";
 
 interface MediaCardProps {
-  image?: {
-    url: string;
-    width?: number | null;
-    height?: number | null;
-    alt: string;
-  };
-  title: string;
-  meta?: ReactNode;
-  href?: string;
-  className?: string;
+    image?: {
+        url: string;
+        width?: number | null;
+        height?: number | null;
+        alt: string;
+    };
+    title: string;
+    meta?: ReactNode;
+    href?: string;
+    className?: string;
 }
 
 export default function MediaCard({
-  image,
-  title,
-  meta,
-  href,
-  className = "",
+    image,
+    title,
+    meta,
+    href,
+    className = "",
 }: MediaCardProps) {
-  const content = (
-    <div
-      className={`flex flex-col gap-2 border border-iplay-white/10 rounded-md shadow-2xl/10 shadow-iplay-grape overflow-hidden ${className}`}
-    >
-      {/* Image */}
-      {image && (
-        <figure className="w-full aspect-square overflow-hidden">
-          <Image
-            src={image.url}
-            alt={image.alt}
-            width={image.width ?? 512}
-            height={image.height ?? 512}
-            className="object-cover w-full h-full"
-            loading="lazy"
-          />
-        </figure>
-      )}
+    const content = (
+        <div
+            className={`flex flex-col gap-2 border border-iplay-white/10 rounded-md shadow-2xl/10 shadow-iplay-grape overflow-hidden ${className}`}
+        >
+            {/* Image */}
 
-      {/* Content */}
-      <div className="flex flex-col gap-1 px-2 pb-4">
-        <strong className="text-lg">{title}</strong>
-        {meta && (
-          <div className="text-sm opacity-70 flex flex-wrap items-center gap-2">
-            {meta}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+            <figure className="flex items-center justify-center w-full aspect-square overflow-hidden border-b border-iplay-white/10">
+                {image ? (
+                    <Image
+                        src={image.url}
+                        alt={image.alt}
+                        width={image.width ?? 512}
+                        height={image.height ?? 512}
+                        className="object-cover w-full h-full"
+                        loading="lazy"
+                    />) :
+                    (<FaMusic className="size-[33%] text-iplay-grape/50"></FaMusic>)}
+            </figure>
 
-  return href ? <Link href={href}>{content}</Link> : content;
+
+            {/* Content */}
+            <div className="flex flex-col gap-1 px-2 pb-4">
+                <strong className="text-lg">{title}</strong>
+                {meta && (
+                    <div className="text-sm opacity-70 flex flex-wrap items-center gap-2">
+                        {meta}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+
+    return href ? <Link href={href}>{content}</Link> : content;
 }
