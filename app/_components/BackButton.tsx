2f16e351df21b4dd07952eab3d3d719f9a6fd0e1
victor-prm <victorpabb@gmail.com>
2026-02-02 23:00:47 +0100

    feat: add back button

---
+++ app/_components/BackButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useRouter, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa";
+
+export default function BackButton() {
+    const router = useRouter();
+    const pathname = usePathname();
+    const [canGoBack, setCanGoBack] = useState(false);
+
+    useEffect(() => {
+        // Disable back button on front page, otherwise enable if history exists
+        setCanGoBack(pathname !== "/" && window.history.length > 1);
+    }, [pathname]);
+
+    return (
+        <button
+            onClick={() => router.back()}
+            style={{
+                visibility: canGoBack ? "visible" : "hidden",
+                pointerEvents: canGoBack ? "auto" : "none",
+            }}
+            className="p-2 rounded cursor-pointer"
+        >
+            <FaArrowLeft />
+        </button>
+    );
+}
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,6 +2,7 @@
 
 import SearchBar from "./search/SearchBar";
 import HeaderTitle from "./HeaderTitle";
+import BackButton from "./BackButton";
 
 export default function Header() {
     const pseudoBlur = "before:absolute before:inset-0 before:-z-10 before:backdrop-blur-3xl"
@@ -9,8 +10,7 @@ export default function Header() {
     return (
         <header className={`h-auto z-100 fixed bg-linear-to-tr from-iplay-black/80 to-iplay-plum/80 p-2 top-0 w-full border-b border-iplay-grape/20 ${pseudoBlur}`}>
             <nav className="relative flex h-full items-center justify-between container mx-auto max-w-300">
-                <div>
-                </div>
+                <BackButton />
                 <HeaderTitle />
                 <SearchBar />
             </nav>
